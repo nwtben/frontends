326beadffe9970bd9ebc76eb05ab5c0316316492
Benjamin Ramnek Holstad <benjamin@nordicwebteam.se>
2023-04-14 16:36:00 +0200

    Testing local font

--- templates/vue-demo-store/nuxt.config.ts
+++ templates/vue-demo-store/nuxt.config.ts
@@ -7,10 +7,20 @@ export default defineNuxtConfig({
     head: {
       link: [
         {
-          rel: "stylesheet",
-          href: "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap",
+          rel: "preload",
+          href: "/assets/fonts/inter-v12-latin.css?&display=swap",
+          as: "style",
+          onload: "this.onload=null;this.rel='stylesheet'"
         },
       ],
+      /*noscript: {
+        link: [
+          {
+            rel: "stylesheet",
+            href: "/assets/fonts/inter-v12-latin.css?&display=swap",
+          },
+        ],
+      }*/
     }
   },
   shopware: {
