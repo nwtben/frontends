24a1d1dfd06051c66e8ca838c7b65af572fd24ea
Aleksadev <aleksa@nordicwebteam.se>
2023-04-20 14:57:58 +0200

    updated issues for product and category

--- templates/vue-demo-store/components/listing-filters/SwFilterBrands.vue
+++ templates/vue-demo-store/components/listing-filters/SwFilterBrands.vue
@@ -1,22 +1,47 @@
 <script setup lang="ts">
-import { ListingFilter } from "@shopware-pwa/types";
+import { AggregationFilterEntityOption, ListingFilter } from "@shopware-pwa/types";
 import { inject, ref } from "vue";
 import { getTranslatedProperty } from "@shopware-pwa/helpers-next";
 import {
   MagnifyingGlassIcon,
 } from '@heroicons/vue/24/outline';
 
-
+const LIMIT = 1;
 const props = defineProps<{
   filter: ListingFilter;
 }>();
-
+const isShowAll = ref<boolean>();
+const searchText = ref<string>('');
 const emits = defineEmits<{
   (e: "select-value", value: { code: string; value: unknown }): void;
 }>();
 const selectedOptionIds = inject<string[]>("selectedOptionIds");
 const filter = computed(() => props.filter);
 
+const elements = computed<AggregationFilterEntityOption[]>(
+  () => {
+    const temp = (filter.value?.options || filter.value?.entities || []) as AggregationFilterEntityOption[];
+    return !searchText.value ? 
+              temp : 
+              temp.filter(x => getTranslatedProperty(x, 'name').toLowerCase().includes(searchText.value.toLowerCase()));
+  });
+
+const showElements = computed(() => {
+  if (!isShowAll.value) {
+    return elements.value.slice(0, LIMIT);
+  } else {
+    return elements.value;
+  }
+});
+
+const toggleViewAll = () => {
+  isShowAll.value = !isShowAll.value;
+}
+
+const searchBrand = (e: any) => {
+  isShowAll.value = false;
+  searchText.value = e.target.value;
+}
 </script>
 
 <template>
@@ -30,6 +55,7 @@ const filter = computed(() => props.filter);
         />
 
         <input
+          @input="searchBrand"
           data-testid="layout-search-input"
           type="text"
           class="text-base text-gray-500 bg-transparent outline-none ml-2"
@@ -39,7 +65,7 @@ const filter = computed(() => props.filter);
     </div>
     <div class="flex flex-col my-2 max-h-[308px] overflow-y-auto">
       <div
-        v-for="option in filter.options || filter.entities"
+        v-for="option in showElements"
         :key="option.id"
         class="flex items-center py-3 pr-2"
       >
@@ -55,6 +81,8 @@ const filter = computed(() => props.filter);
         <span class="ml-auto text-sm font-medium text-gray-500">10</span>
       </div>
     </div>
-    <button class="text-gray-900 font-medium text-base underline">View all brands</button>
+    <button v-if="elements.length > LIMIT" class="text-gray-900 font-medium text-base underline" @click="toggleViewAll">
+      {{ !isShowAll ? 'View all brands' : 'View less brands' }}
+    </button>
   </div>
 </template>
