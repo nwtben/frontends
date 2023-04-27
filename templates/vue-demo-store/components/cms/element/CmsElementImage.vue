322bf0a282bb719243d93011be60b3473168e043
Aleksadev <aleksa@nordicwebteam.se>
2023-04-27 11:27:19 +0200

    TN-25: updated dynamic link

---
+++ templates/vue-demo-store/components/cms/element/CmsElementImage.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+import {
+  CmsElementImage,
+  CmsElementManufacturerLogo,
+  useCmsElementImage,
+} from "@shopware-pwa/composables-next";
+
+const props = defineProps<{
+  content: CmsElementImage | CmsElementManufacturerLogo;
+}>();
+
+const {
+  containerStyle,
+  displayMode,
+  imageContainerAttrs,
+  imageAttrs,
+  imageLink,
+} = useCmsElementImage(props.content);
+</script>
+<template>
+  <!-- TODO: using a tag only works with externalLink, need to improve this element to deal with both internalLink & externalLink -->
+  <component
+    v-if="imageAttrs.src"
+    class="cms-element-image flex relative"
+    :is="imageLink.url ? 'a' : 'div'"
+    :style="containerStyle"
+    v-bind="imageContainerAttrs"
+  >
+    <img
+      :class="{
+        'h-full w-full': true,
+        'absolute inset-0': ['cover', 'stretch'].includes(displayMode),
+        'object-cover': displayMode === 'cover',
+      }"
+      v-bind="imageAttrs"
+      alt="Image link"
+    />
+  </component>
+</template>
