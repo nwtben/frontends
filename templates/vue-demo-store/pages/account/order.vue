f8f8e9fdd0a90758c7297fa2158c236cc44e9b59
Aleksadev <aleksa@nordicwebteam.se>
2023-05-09 15:24:35 +0200

    TN-31: updated comments for account page v0509

--- templates/vue-demo-store/pages/account/order.vue
+++ templates/vue-demo-store/pages/account/order.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
-import SharedAlert from '../../components/shared/SharedAlert.vue';
-import SharedOrders from '../../components/shared/SharedOrders.vue';
+import SharedAlert from "../../components/shared/SharedAlert.vue";
+import SharedOrders from "../../components/shared/SharedOrders.vue";
 
 definePageMeta({
   layout: "account",
 });
 
 const { orders, loadOrders } = useCustomerOrders();
-
+const isLoading = ref(true);
 useBreadcrumbs([
   {
-    name: "Account Overview",
+    name: "My Account",
     path: "/account",
   },
   {
@@ -19,8 +19,11 @@ useBreadcrumbs([
   },
 ]);
 
-await useAsyncData("getOrders", () => {
-  return loadOrders();
+onMounted(async () => {
+  if (orders?.value && Object.keys(orders.value).length === 0) {
+    await loadOrders();
+  }
+  isLoading.value = false;
 });
 </script>
 
@@ -36,8 +39,21 @@ export default {
       <h3 class="mb-4">Orders</h3>
       <p class="text-base">Your recent orders:</p>
     </section>
-    <section>
-      <SharedAlert v-if="!orders?.length" text="You currently have no order history." />
+    <div v-if="isLoading" class="w-full h-full">
+      <div class="flex animate-pulse flex-col items-top h-full space-y-5">
+        <div class="w-full flex flex-row space-x-6">
+          <div class="w-1/6 bg-gray-300 h-15 rounded-md" />
+          <div class="w-1/6 bg-gray-300 h-15 rounded-md" />
+          <div class="w-1/2 bg-gray-200 h-15 rounded-md" />
+          <div class="w-1/6 bg-gray-300 h-15 rounded-md" />
+        </div>
+      </div>
+    </div>
+    <section v-else>
+      <SharedAlert
+        v-if="!orders?.length"
+        text="You currently have no order history."
+      />
       <SharedOrders v-else :orders="orders || []" />
     </section>
   </section>
--- templates/vue-demo-store/pages/account/profile.vue
+++ templates/vue-demo-store/pages/account/profile.vue
@@ -9,7 +9,7 @@ definePageMeta({
 
 useBreadcrumbs([
   {
-    name: "Account Overview",
+    name: "My Account",
     path: "/account",
   },
   {
@@ -30,28 +30,28 @@ export default {
 <template>
   <section class="flex flex-col space-y-10 mb-24">
     <section>
-      <h3 class="mb-4">My profile</h3>
-      <p class="text-base">Check your personal data</p>
+      <h3 class="mb-4">{{$t('my_profile')}}</h3>
+      <p class="text-base">{{$t('check_your_personal_data')}}</p>
     </section>
     <section>
       <AccountPersonalData />
     </section>
     <section>
       <h4 class="text-lg font-medium mb-4">
-        Login data
+          {{$t('login_data') }}
       </h4>
       <p class="text-sm text-gray-900">
         {{ user?.email }}
       </p>
       <div class="flex items-center mt-6">
         <p class="mr-1 text-sm text-gray-700 font-medium">
-          Change e-mail address
+            {{$t('change_email_address') }}
         </p>
         <ChevronDownIcon class="w-5 h-5" />
       </div>
       <div class="flex items-center mt-6">
         <p class="mr-1 text-sm text-gray-700 font-medium">
-          Change password
+            {{$t('change_password') }}
         </p>
         <ChevronDownIcon class="w-5 h-5" />
       </div>
